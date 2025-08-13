52936cf871595fe735978ebfd8541e276d7c645c
Chang Luo <33987852+luochang212@users.noreply.github.com>
2025-08-13 23:12:44 +0800

    update

--- convert.ps1
+++ convert.ps1
@@ -1,5 +1,5 @@
-# 转换当前目录及子目录下的所有文本文件
-Get-ChildItem -Path . -Recurse -File -ErrorAction SilentlyContinue | Where-Object { $_.Extension -match '\.(sh)$' } | ForEach-Object {
+# 将当前目录及子目录下特定后缀的文件转换为 LF 格式
+Get-ChildItem -Path . -Recurse -File -ErrorAction SilentlyContinue | Where-Object { $_.Extension -match '\.(sh|yml|env)$' } | ForEach-Object {
     $content = Get-Content $_.FullName -Raw -Encoding UTF8
     $content = $content -replace "`r`n", "`n"
     Set-Content -Path $_.FullName -Value $content -NoNewline -Encoding UTF8
